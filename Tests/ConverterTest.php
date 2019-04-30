3142ef910fbb7c40061567257154e63fd71fc69a
Dawand Sulaiman <dawand.sulaiman@gmail.com>
2019-04-30 01:03:36 +0100

    updated the test class

--- Tests/ConverterTest.php
+++ Tests/ConverterTest.php
@@ -1,10 +1,9 @@
 <?php
 
-class ConverterTest extends PHPUnit\Framework\TestCase {
-	public function setUp(){
-		
-	}
+use PHPUnit\Framework\TestCase;
 
+class ConverterTest extends TestCase {
+	
 	public function testNumber(){
 		$KC = new KurdishConverter(350);
 		$this->assertEquals('سێ سەد و پەنجا' , $KC->generateText());
